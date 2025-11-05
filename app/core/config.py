0bcae11e6f35d1a406166603a10a9fb3cfb33062
PangoAguirre <bcamilo.aguirre@udea.edu.co>
2025-11-05 17:21:09 -0500

    config update

--- app/core/config.py
+++ app/core/config.py
@@ -1,11 +1,13 @@
 import os
+
 from dotenv import load_dotenv
-from supabase import create_client, Client
+from supabase import Client, create_client
 
 load_dotenv()
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
 
 def get_supabase() -> Client:
     return create_client(SUPABASE_URL, SUPABASE_KEY)
