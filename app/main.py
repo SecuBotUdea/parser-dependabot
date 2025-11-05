ca91f1d1699be0cecfaacab3b38dc3f0c9e4b0db
PangoAguirre <bcamilo.aguirre@udea.edu.co>
2025-11-04 19:03:09 -0500

    lol

--- app/main.py
+++ app/main.py
@@ -1,21 +1,25 @@
 from fastapi import FastAPI
+from fastapi.concurrency import asynccontextmanager
 
+from app.db.connection import close_pool, init_pool
 from app.hooks.webhook import router as webhook_router
 
-"""@asynccontextmanager
+
+@asynccontextmanager
 async def lifespan_handler(app: FastAPI):
     # --- STARTUP (Inicialización de Recursos) ---
     init_pool()
     print("✅ Conexión a la base de datos inicializada.")
 
-    yield # La aplicación se mantiene activa aquí (se procesan las peticiones)
+    yield  # La aplicación se mantiene activa aquí (se procesan las peticiones)
 
     # --- SHUTDOWN (Liberación de Recursos) ---
     close_pool()
-    print("🧹 Conexiones a la base de datos cerradas.")"""
+    print("🧹 Conexiones a la base de datos cerradas.")
+
 
 # 2. Inicializar FastAPI con el handler
-app = FastAPI(title="Parser Dependabot")  # , lifespan=lifespan_handler)
+app = FastAPI(title="Parser Dependabot", lifespan=lifespan_handler)
 
 app.include_router(webhook_router)
 
