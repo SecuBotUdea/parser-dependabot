57b74d96d5cf0e7403f0ac9bd544023900abc3b8
PangoAguirre <bcamilo.aguirre@udea.edu.co>
2025-11-06 11:37:28 -0500

    route webhook

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI(title="Parser Dependabot")
 
-# app.include_router(webhook_router)
+app.include_router(webhook_router)
 
 
 # ---------- Ruta de salud ----------
