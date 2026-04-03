ffaeefac3e455e702f4ab284a00d51048c0fd429
PangoAguirre <bcamilo.aguirre@udea.edu.co>
2026-04-03 12:30:07 -0500

    Implement error handling for Supabase credentials
    Added error handling for missing Supabase configuration.

--- app/core/config.py
+++ app/core/config.py
@@ -5,8 +5,11 @@ from supabase import Client, create_client
 
 load_dotenv()
 
-
 def get_supabase() -> Client:
     SUPABASE_URL = os.getenv("SUPABASE_URL")
     SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+    if not url or not key:
+        raise ValueError("❌ Error: SUPABASE_URL o SUPABASE_KEY no configuradas en el entorno.")
+    
     return create_client(SUPABASE_URL, SUPABASE_KEY)
